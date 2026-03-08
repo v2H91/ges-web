9d48d3dab41ce8fe8464ba04b825e992702ce372
hungvu <hungvu19061991@gmail.com>
2026-03-08 21:47:52 +0700

    correct infor

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showContactModal = false;
+  showTrainingModal = false;
+
+  openContact(event: Event) {
+    event.preventDefault();
+    this.showContactModal = true;
+  }
+
+  openTraining(event: Event) {
+    event.preventDefault();
+    this.showTrainingModal = true;
+  }
+
+  closeModals() {
+    this.showContactModal = false;
+    this.showTrainingModal = false;
+  }
 
 }
